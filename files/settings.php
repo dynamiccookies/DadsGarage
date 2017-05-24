552536ec63db541cc3a712d76c9615951f818420
dynamiccookies <dynamiccookies@gmail.com>
2017-05-24 00:20:35 -0500

    Read Last Commit

--- files/settings.php
+++ files/settings.php
@@ -1,25 +1,52 @@
 <?php
 	if (!file_exists("config.ini.php")) {
-		$str="<?php \n/*;Commented line start with ';'\n[connection]\ndbname = \nhost = \nusername = \npassword = \nbranch = \n*/\n?>";
+		$str="<?php \n/*;\n[connection]\ndbname = \nhost = \nusername = \npassword = \nbranch = \n*/\n?>";
 		file_put_contents("config.ini.php", $str);
 	} 
-	$ini = parse_ini_file("config.ini.php");
 	if(isset($_POST['Submit'])) {
-		$str=file_get_contents("config.ini.php");
-		$str=str_replace("dbname = ".$ini["dbname"],"dbname = ".$_POST["dbname"],$str);
-		$str=str_replace("host = ".$ini["host"],"host = ".$_POST["host"],$str);
-		$str=str_replace("username = ".$ini["username"],"username = ".$_POST["username"],$str);
-		$str=str_replace("password = ".$ini["password"],"password = ".$_POST["password"],$str);
-		$str=str_replace("branch = ".$ini["branch"],"branch = ".$_POST["branch"],$str);
+		$str="<?php \n/*;\n[connection]\ndbname = ".$_POST["dbname"]."\nhost = ".$_POST["host"]."\nusername = ".$_POST["username"]."\npassword = ".$_POST["password"]."\nbranch = ".$_POST["branch"]."\n*/\n?>";
 		file_put_contents("config.ini.php", $str);
 	}
+	$ini = parse_ini_file("config.ini.php");
+	require_once("dbcheck.php");
 ?>
-<div style="text-align:center;font-weight:bold;">Settings Page
-<form action="" method="post">
-<br/>Database Name: <input name="dbname" type="textbox" value="<?php echo ($_POST["dbname"]?:$ini["dbname"]);?>">
-<br/>Host Name: <input name="host" type="textbox" value="<?php echo ($_POST["host"]?:$ini["host"]);?>">
-<br/>Username: <input name="username" type="textbox" value="<?php echo ($_POST["username"]?:$ini["username"]);?>">
-<br/>Password: <input name="password" type="textbox" value="<?php echo ($_POST["password"]?:$ini["password"]);?>">
-<br/>Git Branch: <input name="branch" type="textbox" value="<?php echo ($_POST["branch"]?:$ini["branch"]);?>">
-<br/><input type="Submit" name="Submit" value="Submit"></form>
-</div>
+<head>
+	<style>
+		div:first-child {font-size:36px;font-weight:bold;}
+		table {margin:auto;}
+		td:first-child {text-align:right;font-weight:bold;}
+		input[type=textbox] {width:400px;}
+	</style>
+</head>
+<body>
+	<div style="text-align:center;">Settings Page
+	<form action="" method="post">
+	<table>
+		<tr>
+			<td>Host Name:</td>
+			<td><input name="host" type="textbox" value="<?php echo $ini["host"];?>"></td>
+			<td><?php echo ($array["connTest"]?:"No Array");?></td>
+		</tr>
+		<tr>
+			<td nowrap>Database Name:</td>
+			<td><input name="dbname" type="textbox" value="<?php echo $ini["dbname"];?>"></td>
+			<td><?php echo ($array["dbTest"]?:"No Array");?></td>
+		</tr>
+		<tr>
+			<td>Username:</td>
+			<td><input name="username" type="textbox" value="<?php echo $ini["username"];?>"></td>
+			<td rowspan=2><?php echo ($array["credTest"]?:"No Array");?></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input name="password" type="textbox" value="<?php echo $ini["password"];?>"></td>
+		</tr>
+		<tr>
+			<td>Git Branch:</td>
+			<td><input name="branch" type="textbox" value="<?php echo $ini["branch"];?>"></td>
+			<td></td>
+		</tr>
+	</table>
+	<br/><input type="Submit" name="Submit" value="Submit"></form>
+	</div>
+</body>
