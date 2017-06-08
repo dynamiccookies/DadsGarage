31877890c1b1fd9746fb2b97d4952a09450ebaf6
dynamiccookies <dynamiccookies@gmail.com>
2017-06-07 23:17:14 -0500

    Fix File Upload
    File Upload failed if vehicles folder didn't exist.
    Also simplified code on files/menu.php.

--- files/menu.php
+++ files/menu.php
@@ -2,11 +2,4 @@
 <a href="../admin">Admin</a>
 <a href="../files/settings.php">Settings</a>
 <a href="#">Customers</a><br/><br/>
-<a href="<?php
-	//Set path to 'admin'
-	$admin=substr(getcwd(), strrpos(getcwd(), '/') + 1);
-	if ($admin=="files") {$admin="../admin/";} 
-	elseif ($admin=="admin") {$admin="";} 
-	else {$admin="admin/";}
-	echo $admin;
-?>secure.php?logout=1">Logout</a>
+<a href="../admin/secure.php?logout=1">Logout</a>
