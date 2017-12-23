7cd27975fd44b025c46a5528838f233230933217
dynamiccookies <dynamiccookies@gmail.com>
2017-12-23 12:41:33 -0600

    Update Header
    Changed Doctype and added jquery reference

--- files/header.php
+++ files/header.php
@@ -2,9 +2,10 @@
 	$path=substr(getcwd(), strrpos(getcwd(), '/') + 1);
 	if ($path=="admin") {$path="../files/style.css";} elseif ($path=="files") {$path="style.css";} else {$path="files/style.css";}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<?php if ($vehicle) {echo "<title>".$vehicle."</title>";} else {echo "<title>Dad's Garage</title>";}?>
 		<link rel="stylesheet" type="text/css" href="<?php echo $path;?>">
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 	</head>
