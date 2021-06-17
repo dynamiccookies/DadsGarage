b2068921a24bcafc353a51924825834629d4a16b
dynamiccookies <dynamiccookies@users.noreply.github.com>
2021-06-16 22:20:16 -0500

    Update header.php
    Rename $path variable to $css
    Update spacing when concatenating strings and around equals signs
    Replace double quotes with single quotes where necessary

--- files/header.php
+++ files/header.php
@@ -1,11 +1,11 @@
 <?php
-	$path=substr(getcwd(), strrpos(getcwd(), '/') + 1);
-	if ($path=="admin") {$path="../files/style.css";} elseif ($path=="files") {$path="style.css";} else {$path="files/style.css";}
+	$css = substr(getcwd(), strrpos(getcwd(), '/') + 1);
+	if ($css == 'admin') {$css = '../files/style.css';} elseif ($css == 'files') {$css = 'style.css';} else {$css = 'files/style.css';}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
-		<?php if ($vehicle) {echo "<title>".$vehicle."</title>";} else {echo "<title>Dad's Garage</title>";}?>
-		<link rel="stylesheet" type="text/css" href="<?php echo $path;?>">
-		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<?php if ($vehicle) {echo '<title>' . $vehicle . '</title>';} else {echo "<title>Dad's Garage</title>";}?>
+		<link rel='stylesheet' type='text/css' href='<?php echo $css ;?>'>
+		<script src='http://code.jquery.com/jquery-latest.min.js'></script>
 	</head>
