b3c5bf43c88edf1d76aeea33090bd82cf2ad25db
dynamiccookies <dynamiccookies@users.noreply.github.com>
2021-07-02 18:15:51 -0500

    Rewrote hybrid PHP/HTML to PHP
    - Rewrote hybrid code as all PHP
    - Removed unnecessary commented out code
    - removed `logout` querystring's parameter as it's not needed

--- files/menu.php
+++ files/menu.php
@@ -1,5 +1,7 @@
-<a href="../">Home</a>
-<a href="../admin">Admin</a>
-<a href="../files/settings.php">Settings</a>
-<!--<a href="#">Customers</a>--><br/><br/>
-<a href="../admin/secure.php?logout=1">Logout</a>
+<?php
+
+	echo "<a href='../'>Home</a>\n";
+	echo "<a href='../admin'>Admin</a>\n";
+	echo "<a href='../files/settings.php'>Settings</a>\n<br/><br/>\n";
+	echo "<a href='../admin/secure.php?logout'>Logout</a>";
+?>
