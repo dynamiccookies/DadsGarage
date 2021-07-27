37d486a9732df420e886629f9abb1430b3ae9755
dynamiccookies <dynamiccookies@users.noreply.github.com>
2021-07-27 13:24:32 -0500

    Update header.php
    - Replaced usage of `substr` and `strrpos` with `preg_match`
    - Combined admin and files directories value
    - Replaced `<?php echo` with `<?=`

--- files/header.php
+++ files/header.php
@@ -1,12 +1,12 @@
 <?php
 
-	$css = substr(getcwd(), strrpos(getcwd(), '/') + 1);
-	if($css == 'admin') {$css = '../files/';} elseif($css == 'files') {$css = '';} else {$css = 'files/';}
+	if (preg_match('/admin|files/', getcwd())) {$css = '../';}
+	else {$css = '';}
 ?>
 <!DOCTYPE html>
 <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
 	<head>
 		<?php if(isset($vehicle)) {echo '<title>' . $vehicle . '</title>';} else {echo "<title>Dad's Garage</title>";}?>
-		<link rel='stylesheet' type='text/css' href='<?php echo $css . 'style.css';?>'>
-		<script src='http://code.jquery.com/jquery-latest.min.js'></script>
+		<link rel='stylesheet' type='text/css' href='<?= $css . 'files/style.css';?>'>
+		<script src='//code.jquery.com/jquery-latest.min.js'></script>
 	</head>
