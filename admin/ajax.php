6080fac0e2af528f29d515d3b4872be11f53d563
dynamiccookies <dynamiccookies@users.noreply.github.com>
2021-07-28 17:47:16 -0500

    Delete `ajax.php` & `conn.php`
    - Moved contents of `conn.php` into `include.php`
    - Deleted `conn.php`
    - Deleted `ajax.php` since it wasn't used
    - Removed `require` line including `conn.php`
    
    Close #58

--- admin/ajax.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	if(!isset($_SESSION)){session_start();} 
-	if(isset($_POST['view'])) {
-		$_SESSION['view']=$_POST['view'];
-	} else { $_SESSION['view']='forsale';}
-?>
--- files/include.php
+++ files/include.php
@@ -27,7 +27,16 @@
 	if ($currentPath == 'files') {$admin = '../admin/';} elseif ($currentPath == 'admin') {$admin = '';} else {$admin = 'admin/';}
 	
 
-	require $files . 'conn.php';
+	// If the 'config.ini.php' file does not exist, redirect to the 'settings.php' page so it can be created
+	if (!(file_exists($files . 'config.ini.php'))) {echo "<meta http-equiv=refresh content=\"0; URL=" . $files . "settings.php\">";} 
+
+	// Store the contents of 'config.ini.php' into the $ini variable
+	$ini = parse_ini_file($files . 'config.ini.php');
+
+	// Try to create a connection to the database with the username and password stored in the 'config.ini.php' file
+	try {$db = new PDO('mysql:dbname=' . $ini['dbname'] . ';host=' . $ini['host'], $ini['username'], $ini['password']);} 
+	catch (Exception $e) {echo 'Caught exception: ',  $e->getMessage(), '\n';}
+
 
 	$success = 'noscreen ';
 
